6ef2745fecedcc6b0cdee8f3ba4221e0891ac0ca
Mark Dobossy <mark@dobossy.com>
2018-11-06 19:21:22 -0600

    Added capacitor, updated StatusBar and SplashScreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,8 +16,6 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AddMemoryPageModule],
   providers: [
-    StatusBar,
-    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
